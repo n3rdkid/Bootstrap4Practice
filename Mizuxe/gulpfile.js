1c1ebda1c7525e7c5352edbc4753fe1aefbaf1a1
Saurav Adhikari <sauravads123@gmail.com>
2019-05-01 15:39:49 +0545

    Implement Nav bar and minor bug fixes

--- Mizuxe/gulpfile.js
+++ Mizuxe/gulpfile.js
@@ -1,15 +1,20 @@
 const gulp=require('gulp');
 const gulpSass=require('gulp-sass');
-const gulpBrowserSync=require('browser-sync').create();
+const browserSync=require('browser-sync').create();
 
 function sass()
 {
-  return gulp.src(['node_modules/bootstrap/scss/bootstrap.scss','src/scss/style.scss'])
+  return gulp.src(['node_modules/bootstrap/scss/bootstrap.scss','./src/scss/*.scss'])
   .pipe(gulpSass({
       outputStyle: 'expanded'
   }))
   .pipe(gulp.dest('src/css'))
   .pipe(browserSync.stream());
+}
+function js(){
+    return gulp.src(['node_modules/bootstrap/dist/js/bootstrap.min.js', 'node_modules/jquery/dist/jquery.min.js', 'node_modules/popper.js/dist/umd/popper.min.js'])
+        .pipe(gulp.dest("src/js"))
+        .pipe(browserSync.stream());
 }
 function watch()
 {
@@ -24,3 +29,4 @@ function watch()
 
 exports.sass = sass;
 exports.watch = watch;
+exports.js=js;
