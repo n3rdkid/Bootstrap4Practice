c7960d3d3a9b1162a1e1dfd76aa8597715c5e771
Saurav Adhikari <sauravads123@gmail.com>
2019-05-01 14:39:35 +0545

    Setup Mizuxe starter

---
+++ Mizuxe/gulpfile.js
@@ -0,0 +1,21 @@
+const gulp=require('gulp');
+const gulpSass=require('gulp-sass');
+const gulpBrowserSync=require('browser-sync').init();
+
+function sass()
+{
+  return gulp.src(['node_modules/bootstrap/scss/bootstrap.scss','src/scss/style.scss'])
+  .pipe(gulpSass({
+      outputStyle: 'expanded'
+  }))
+  .pipe(gulp.dest('src/css'))
+  .pipe(browserSync.stream());
+}
+function watch()
+{
+  gulp.watch(['node_modules/bootstrap/scss/bootstrap.scss','src/scss/*.scss'], sass);
+  gulp.watch('*.html').on('change',browserSync.reload);
+}
+
+exports.sass = sass;
+exports.watch = watch;
