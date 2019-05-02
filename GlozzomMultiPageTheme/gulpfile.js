c7f0344a8e79b0d4cb84141b9ec2d645ea14287a
Saurav Adhikari <sauravads123@gmail.com>
2019-05-02 19:51:52 +0545

    Inital Skeleton

---
+++ GlozzomMultiPageTheme/gulpfile.js
@@ -0,0 +1,32 @@
+const gulp=require('gulp');
+const gulpSass=require('gulp-sass');
+const browserSync=require('browser-sync').create();
+
+function sass()
+{
+  return gulp.src(['node_modules/bootstrap/scss/*.scss','./src/scss/*.scss'])
+  .pipe(gulpSass({
+      outputStyle: 'expanded'
+  }))
+  .pipe(gulp.dest('src/css'))
+  .pipe(browserSync.stream());
+}
+function js(){
+    return gulp.src(['node_modules/bootstrap/dist/js/bootstrap.min.js', 'node_modules/jquery/dist/jquery.min.js', 'node_modules/popper.js/dist/umd/popper.min.js'])
+        .pipe(gulp.dest("src/js"))
+        .pipe(browserSync.stream());
+}
+function watch()
+{
+  browserSync.init({
+    server: {
+        baseDir: "./"
+    }
+});
+  gulp.watch(['node_modules/bootstrap/scss/*.scss','src/scss/*.scss'], sass);
+  gulp.watch('*.html').on('change',browserSync.reload);
+}
+
+exports.sass = sass;
+exports.watch = watch;
+exports.js=js;
